93fbdc6e3c5e00332a9817da706336cee6928b3e
raofen <trout.kazu@gmail.com>
2025-08-09 17:24:03 +0900

    第3章まで完了

---
+++ app/_components/Header/index.tsx
@@ -0,0 +1,15 @@
+// Header
+
+import Image from "next/image";
+import styles from "./index.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.logoLink}>
+        <Image src="/logo.svg" alt="SIMPLE ホーム" className={styles.logo} width={348} height={133} priority />
+      </Link>
+    </header>
+  );
+}
---
+++ app/_libs/microcms.ts
@@ -0,0 +1,15 @@
+// microcms.ts
+
+export type Category = {
+  name: string;
+};
+
+export type News = {
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+};
